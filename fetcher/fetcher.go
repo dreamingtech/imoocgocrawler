7cc4f010c31a0456a21c054205a9a8773b98f034
DreamingTech <26395688+dreamingtech@users.noreply.github.com>
2023-07-19 09:02:11 +0800

    1. 给 ParseCityList 添加测试 2. 解决 determineEncoding 中 Peek 后引起的 BUG 3. 解决 Fetch 中使用默认的 http.Get 方法导致的获取的源码与浏览器中获取的源码不同的 BUG

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -13,8 +13,11 @@ import (
 	"net/http"
 )
 
-func determineEncoding(reader io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(reader).Peek(1024)
+func determineEncoding(reader *bufio.Reader) encoding.Encoding {
+	// Peek 是对 reader 的一个预读, 读取前 1024 个字节
+	// 在后面 `reader := transform.NewReader(resp.Body, e.NewDecoder())` 中已经读取的 1024 个字节就不会再读取了
+	// 为了解决这个问题, 可以把 bufio.NewReader 的整体作为参数传递进来
+	bytes, err := reader.Peek(1024)
 	if err != nil {
 		// 如果出错, 就返回默认的 utf-8 编码
 		log.Printf("error in determineEncoding: %v", err)
@@ -26,23 +29,39 @@ func determineEncoding(reader io.Reader) encoding.Encoding {
 }
 
 func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+	// resp, err := http.Get(url)
+
+	// 使用 http.Get 获取到的响应和浏览器中看到的响应不同, 所以要自定义请求头
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Add(
+		"User-Agent",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+	resp, err := http.DefaultClient.Do(request)
 
 	if err != nil {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("error in defer Body.Close(): %v", err)
+		}
+	}(resp.Body)
 
 	if resp.StatusCode != http.StatusOK {
 		// return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
 		return nil, errors.New(fmt.Sprintf("wrong status code: %d", resp.StatusCode))
 	}
 
-	e := determineEncoding(resp.Body)
+	// 把 bufio.NewReader(resp.Body) 作为参数传递进去, 利用 go 语言值传递的特性,
+	// 解决 peek 读取了 1024 个字节后, 后面的 transform.NewReader(resp.Body, e.NewDecoder()) 就不会再读取的问题
+	bodyReader := bufio.NewReader(resp.Body)
 
-	reader := transform.NewReader(resp.Body, e.NewDecoder())
+	e := determineEncoding(bodyReader)
 
-	return io.ReadAll(reader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+
+	return io.ReadAll(utf8Reader)
 
 }
---
+++ parser/zhenai/citylist_test.go
@@ -0,0 +1,54 @@
+package parser
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	// 测试时, 要尽量避免因为网络等外部因素引起的可能的错误
+	// 可以把 html 保存到本地, 使用文件读取的方式, 而是不发送网络请求
+	// html, err := fetcher.Fetch("https://www.zhenai.com/zhenghun")
+	// html, err := fetcher.Fetch("http://localhost:8080/mock/www.zhenai.com/zhenghun")
+
+	html, err := os.ReadFile("citylist_test_data.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	result := ParseCityList(html)
+
+	// verify result
+	const resultSize = 470
+
+	if len(result.Requests) != resultSize {
+		t.Errorf("result should have %d requests; but had %d", resultSize, len(result.Requests))
+	}
+
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d items; but had %d", resultSize, len(result.Items))
+	}
+
+	expectedUrls := []string{
+		"http://www.zhenai.com/zhenghun/aba",
+		"http://www.zhenai.com/zhenghun/akesu",
+		"http://www.zhenai.com/zhenghun/alashanmeng",
+	}
+	expectedCities := []string{
+		"阿坝", "阿克苏", "阿拉善盟",
+	}
+
+	for i, url := range expectedUrls {
+		if result.Requests[i].Url != url {
+			t.Errorf("expected url #%d: %s; but was %s", i, url, result.Requests[i].Url)
+		}
+	}
+
+	for i, city := range expectedCities {
+		// items 中的元素类型是 interface{}, 需要使用类型断言
+		if result.Items[i].(string) != city {
+			t.Errorf("expected city #%d: %s; but was %s", i, city, result.Items[i].(string))
+		}
+	}
+}
