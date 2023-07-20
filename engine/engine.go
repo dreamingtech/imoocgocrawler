35c04c2fe4c28313cb3458666034f41d4b7dde6c
DreamingTech <26395688+dreamingtech@users.noreply.github.com>
2023-07-20 09:36:32 +0800

    抽象出 Worker 的概念, 发送请求并解析响应

--- engine/engine.go
+++ engine/engine.go
@@ -5,6 +5,21 @@ import (
 	"log"
 )
 
+func worker(r Request) (ParseResult, error) {
+
+	log.Printf("Fetching url: %s", r.Url)
+
+	body, err := fetcher.Fetch(r.Url)
+
+	if err != nil {
+		log.Printf("Fetcher url error. url: %s, error: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+
+	// 调用解析函数, 得到解析结果
+	return r.ParserFunc(body), nil
+}
+
 func Run(seeds ...Request) {
 	var requests []Request
 	for _, r := range seeds {
@@ -16,17 +31,10 @@ func Run(seeds ...Request) {
 		r := requests[0]
 		requests = requests[1:]
 
-		log.Printf("Fetching url: %s", r.Url)
-
-		body, err := fetcher.Fetch(r.Url)
-
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher url error. url: %s, error: %v", r.Url, err)
 			continue
 		}
-
-		// 调用解析函数, 得到解析结果
-		parseResult := r.ParserFunc(body)
 
 		// 将解析结果中的 Requests 添加到 requests 列表中
 		requests = append(requests, parseResult.Requests...)
