1fd75a6cbef7622ecb26268a3c2df9c2341e381f
DreamingTech <26395688+dreamingtech@users.noreply.github.com>
2023-07-28 09:32:44 +0800

    验证保存到 es 中的数据

--- persist/itemsaver.go
+++ persist/itemsaver.go
@@ -1,7 +1,6 @@
 package persist
 
 import (
-	"fmt"
 	"github.com/olivere/elastic/v7"
 	"golang.org/x/net/context"
 	"log"
@@ -25,24 +24,25 @@ func ItemSaver() chan interface{} {
 }
 
 // 可以调用 http.Post 发送请求保存数据, 也可以使用 elasticsearch 的 api 保存数据
-func save(item interface{}) {
+// 为了能够测试已经保存的数据是否可以被取出来并被解析为 profile 的结构, 要返回保存到 es 时的 id
+func save(item interface{}) (id string, err error) {
 	client, err := elastic.NewClient(
 		elastic.SetURL("http://127.0.0.1:9200"),
 		// Must turn off sniff in docker
 		elastic.SetSniff(false),
 	)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 	// client.Index().Index("dating_profile").Type("zhenai").Id("abc").BodyJson(item).Do(context.Background())
 	// 不设置 id 时, es 会自动创建 id
-
+	// todo 'Type' is deprecated
 	resp, err := client.Index().Index("dating_profile").Type("zhenai").BodyJson(item).Do(context.Background())
 
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	fmt.Printf("resp: %#v", resp)
+	return resp.Id, nil
 
 }
--- persist/itemsaver_test.go
+++ persist/itemsaver_test.go
@@ -1,12 +1,16 @@
 package persist
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	model "github.com/dreamingtech/imoocgocrawler/model/zhenai"
+	"github.com/olivere/elastic/v7"
 	"testing"
 )
 
 func TestItemSaver(t *testing.T) {
-	profile := model.Profile{
+	expected := model.Profile{
 		Age:        34,
 		Height:     162,
 		Weight:     57,
@@ -21,5 +25,44 @@ func TestItemSaver(t *testing.T) {
 		Education:  "大学本科",
 		Car:        "未购车",
 	}
-	save(profile)
+	id, err := save(expected)
+	if err != nil {
+		panic(err)
+	}
+
+	// 测试中依赖外界环境, 如果没有启动 es docker, 测试就会失败, 可以使用 docker go client 来启动 docker es
+	// todo Try to start up elasticsearch using docker go client
+	// 从 es 中获取数据
+	client, err := elastic.NewClient(
+		elastic.SetURL("http://127.0.0.1:9200"),
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Get().Index("dating_profile").Type("zhenai").Id(id).Do(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+	// resp test &{Index:dating_profile Type:zhenai Id:YjYGmokB60J7JRB5eV16
+	// Uid: Routing: Parent: Version:0xc00001ae00 SeqNo:0xc00001ae08 PrimaryTerm:0xc00001ae18
+	// Source:[123 34 110 ... 232 180 173 232 189 166 34 125] Found:true Fields:map[] Error:<nil>}
+	// fmt.Printf("resp test %+v", resp)
+	// 返回的数据在 source 中
+	// resp test {"name":"安静的雪","gender":"女","age":34,"height":162,"weight":57,"income":"3001-5000元",
+	// "marriage":"离异","education":"大学本科","occupation":"人事/行政","hokou":"山东菏泽","xingzuo":"牡羊座","house":"已购房","car":"
+	fmt.Printf("resp test %s\n", resp.Source)
+
+	var actual model.Profile
+	err = json.Unmarshal(resp.Source, &actual)
+	if err != nil {
+		panic(err)
+	}
+
+	if actual != expected {
+		t.Errorf("got %v, expected %v", actual, expected)
+	} else {
+		fmt.Println("actual equals expected, pass")
+	}
 }
