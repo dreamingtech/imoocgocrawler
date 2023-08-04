ceee0dcc56b9e4ef6a5dcc17afc424ec53c82e92
DreamingTech <26395688+dreamingtech@users.noreply.github.com>
2023-08-04 09:55:34 +0800

    1. 添加首页 index.html 2. 移动 static 到 view 目录中, 以便 http.FileServer 把首页当成静态文件处理

--- frontend/starter.go
+++ frontend/starter.go
@@ -10,7 +10,7 @@ func main() {
 	// 解决 js 和 css 文件无法加载的问题
 	// /search 接口只处理 search 请求, 其他的请求都未处理, 所以无法加载 js 和 css 文件
 	// 因为我们的 js 和 css starter 同级的 static 目录中, 所以设置目录为当前目录即可
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/", http.FileServer(http.Dir("view/")))
 
 	// curl --location 'localhost:9999/search?q=%E7%94%B7%20%E6%9C%89%E6%88%BF&from=20' \
 	// --header 'Content-Type: application/json'
